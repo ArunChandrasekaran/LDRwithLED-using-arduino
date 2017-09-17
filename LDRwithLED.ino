9c50036f2fa4988af023614a3ac229ce641bbd3e
ArunChandrasekaran <unleasharun@gmail.com>
2017-09-17 23:06:15 +0530

    first commit

---
+++ LDRwithLED.ino
@@ -0,0 +1,34 @@
+const int ledPin = 13;   
+const int ldrPin = A0;  
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+  pinMode(ldrPin, INPUT);  
+}
+
+void loop() {
+
+  int ldrStatus = analogRead(ldrPin);   
+  
+   if (ldrStatus <=300) {
+
+    digitalWrite(ledPin, HIGH);               //turn LED on
+    Serial.println("LDR is DARK, LED is ON");
+    
+   }
+  else {
+
+    digitalWrite(ledPin, LOW);          //turn LED off
+    Serial.println("---------------");
+  }
+
+
+
+  
+}
+
+
+
+
